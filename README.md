2e2caae80267751a9a9dc8e7d435a39c3b86cd2c
Rob Parrett <robparrett@gmail.com>
2022-04-01 15:31:02 -0700

    Automatically run examples and update readme

---
+++ README.md
@@ -0,0 +1,9 @@
+## Commit Tested
+648544fa Bump actions/cache from 2 to 3 (#4348)
+
+## Results 
+|example|status|
+|-|-|
+|hello_world|:heavy_check_mark:|
+|contributors|:heavy_check_mark:|
+|many_sprites|:heavy_check_mark:|
--- get_examples/src/main.rs
+++ get_examples/src/main.rs
@@ -1,5 +1,7 @@
 use serde_derive::Deserialize;
-use std::fs;
+use std::io::Write;
+use std::process::Command;
+use std::{fs, io};
 
 #[derive(Debug, Deserialize)]
 struct Example {
@@ -12,6 +14,16 @@ struct Cargo {
     example: Vec<Example>,
 }
 
+fn get_current_commit_string() -> String {
+    let output = Command::new("git")
+        .current_dir(std::fs::canonicalize("../bevy").unwrap())
+        .args(["log", "--oneline", "-n", "1"])
+        .output()
+        .expect("failed to execute process");
+
+    String::from_utf8(output.stdout).expect("Failed to parse git output")
+}
+
 fn main() {
     let ignore = ["android", "custom_loop"];
 
@@ -19,11 +31,50 @@ fn main() {
 
     let decoded: Cargo = toml::from_str(&toml_str).unwrap();
 
-    for example in decoded.example {
+    let mut table_lines = vec![];
+    table_lines.push("## Commit Tested".to_string());
+    table_lines.push(get_current_commit_string());
+    table_lines.push("## Results ".to_string());
+    table_lines.push("|example|status|".to_string());
+    table_lines.push("|-|-|".to_string());
+
+    for example in decoded.example.iter().take(3) {
         if ignore.iter().any(|i| example.path.contains(i)) {
             continue;
         }
 
-        println!("{}", example.name);
+        let example_config = format!("../config/{}.ron", example.name);
+        let config = if std::path::Path::new(&example_config).exists() {
+            example_config
+        } else {
+            "../config/default.ron".to_string()
+        };
+
+        let output = Command::new("xvfb")
+            .current_dir(std::fs::canonicalize("../bevy").unwrap())
+            .env("CI_TESTING_CONFIG", config)
+            .args([
+                "cargo",
+                "run",
+                "--example",
+                &example.name,
+                "--features=x11,bevy_ci_testing",
+            ])
+            .output()
+            .expect("failed to execute process");
+
+        println!("{} {:?}", example.name, output.status);
+        io::stdout().write_all(&output.stdout).unwrap();
+        io::stderr().write_all(&output.stderr).unwrap();
+
+        let status_string = if output.status.success() {
+            ":heavy_check_mark:".to_string()
+        } else {
+            format!(":x: (Code {})", output.status.code().unwrap())
+        };
+
+        table_lines.push(format!("|{}|{}|", example.name, status_string));
     }
+
+    std::fs::write("../README.md", table_lines.join("\n")).expect("Failed to write readme");
 }
