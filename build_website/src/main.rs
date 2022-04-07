765b5f9e6581d0a7da4bf8d48e23c5b8e65620ef
Rob Parrett <robparrett@gmail.com>
2022-04-07 15:24:27 -0700

    Add summary

--- build_website/src/main.rs
+++ build_website/src/main.rs
@@ -4,15 +4,26 @@ use tera::{Context, Tera};
 fn main() {
     let runs = metadata::load(10);
 
+    let mut num_fails = vec![];
+
     let mut all_examples: HashSet<String> = HashSet::new();
     for run in runs.iter() {
-        for (example, _) in run.results.iter() {
+        let mut fails = 0;
+
+        for (example, result) in run.results.iter() {
+            if result.code != 0 {
+                fails += 1;
+            }
+
             all_examples.insert(example.clone());
         }
+
+        num_fails.push(fails);
     }
 
     let mut context = Context::new();
     context.insert("runs".to_string(), &runs);
+    context.insert("num_fails".to_string(), &num_fails);
     context.insert("examples".to_string(), &all_examples);
 
     let rendered = Tera::one_off(
